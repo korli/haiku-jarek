fd0bf5d8389d1e04015a3d469447f8958fc42d35
Jaroslaw Pelczar <jarek@jpelczar.com>
2018-03-10 14:37:50 +0100

    runtime_loader: Add dummy arch_relocate.cpp on AArch64
    AArch64 version now links
    
    Signed-off-by: Jaroslaw Pelczar <jarek@jpelczar.com>

---
+++ src/system/runtime_loader/arch/aarch64/arch_relocate.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018, Jaroslaw Pelczar <jarek@jpelczar.com>
+ * Distributed under the terms of the MIT License.
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "runtime_loader_private.h"
+
+#include <runtime_loader.h>
+
+//#define TRACE_RLD
+#ifdef TRACE_RLD
+#	define TRACE(x) dprintf x
+#else
+#	define TRACE(x) ;
+#endif
+
+status_t
+arch_relocate_image(image_t *rootImage, image_t *image,
+	SymbolLookupCache* cache)
+{
+	debugger("arch_relocate_image: Not Yet Implemented!");
+	return B_OK;
+}
