3e4099ee95fecb2eee22284c8299309a0b18a6ec
Jaroslaw Pelczar <jarek@jpelczar.com>
2018-04-05 18:14:45 +0200

    AArch64: Initial VFP save and restore functions
    Signed-off-by: Jaroslaw Pelczar <jarek@jpelczar.com>

--- src/system/kernel/arch/aarch64/arch_int.cpp
+++ src/system/kernel/arch/aarch64/arch_int.cpp
@@ -17,6 +17,69 @@
 
 #include <stdio.h>
 
+#include <kernel/arch/aarch64/armreg.h>
+
+static void save_vfp_context(struct vfp_context *state)
+{
+	__uint128_t *vfp_state;
+	uint64 fpcr, fpsr;
+
+	vfp_state = state->regs;
+	__asm __volatile(
+	    "mrs	%0, fpcr		\n"
+	    "mrs	%1, fpsr		\n"
+	    "stp	q0,  q1,  [%2, #16 *  0]\n"
+	    "stp	q2,  q3,  [%2, #16 *  2]\n"
+	    "stp	q4,  q5,  [%2, #16 *  4]\n"
+	    "stp	q6,  q7,  [%2, #16 *  6]\n"
+	    "stp	q8,  q9,  [%2, #16 *  8]\n"
+	    "stp	q10, q11, [%2, #16 * 10]\n"
+	    "stp	q12, q13, [%2, #16 * 12]\n"
+	    "stp	q14, q15, [%2, #16 * 14]\n"
+	    "stp	q16, q17, [%2, #16 * 16]\n"
+	    "stp	q18, q19, [%2, #16 * 18]\n"
+	    "stp	q20, q21, [%2, #16 * 20]\n"
+	    "stp	q22, q23, [%2, #16 * 22]\n"
+	    "stp	q24, q25, [%2, #16 * 24]\n"
+	    "stp	q26, q27, [%2, #16 * 26]\n"
+	    "stp	q28, q29, [%2, #16 * 28]\n"
+	    "stp	q30, q31, [%2, #16 * 30]\n"
+	    : "=&r"(fpcr), "=&r"(fpsr) : "r"(vfp_state));
+
+	state->fpcr = fpcr;
+	state->fpsr = fpsr;
+}
+
+static void load_vfp_context(struct vfp_context *state)
+{
+	__uint128_t *vfp_state;
+	uint64 fpcr, fpsr;
+
+	vfp_state = state->regs;
+	fpcr = state->fpcr;
+	fpsr = state->fpsr;
+
+	__asm __volatile(
+	    "ldp	q0,  q1,  [%2, #16 *  0]\n"
+	    "ldp	q2,  q3,  [%2, #16 *  2]\n"
+	    "ldp	q4,  q5,  [%2, #16 *  4]\n"
+	    "ldp	q6,  q7,  [%2, #16 *  6]\n"
+	    "ldp	q8,  q9,  [%2, #16 *  8]\n"
+	    "ldp	q10, q11, [%2, #16 * 10]\n"
+	    "ldp	q12, q13, [%2, #16 * 12]\n"
+	    "ldp	q14, q15, [%2, #16 * 14]\n"
+	    "ldp	q16, q17, [%2, #16 * 16]\n"
+	    "ldp	q18, q19, [%2, #16 * 18]\n"
+	    "ldp	q20, q21, [%2, #16 * 20]\n"
+	    "ldp	q22, q23, [%2, #16 * 22]\n"
+	    "ldp	q24, q25, [%2, #16 * 24]\n"
+	    "ldp	q26, q27, [%2, #16 * 26]\n"
+	    "ldp	q28, q29, [%2, #16 * 28]\n"
+	    "ldp	q30, q31, [%2, #16 * 30]\n"
+	    "msr	fpcr, %0		\n"
+	    "msr	fpsr, %1		\n"
+	    : : "r"(fpcr), "r"(fpsr), "r"(vfp_state));
+}
 extern "C" void do_el1h_sync(iframe * frame)
 {
 	for(;;);
